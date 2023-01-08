05308cd483f595fa505fa3b675da58d96755f37f
이윤택 <eeyoontaek@iyuntaeg-ui-MacBookPro.local>
2023-01-08 15:54:22 +0900

    FIX error

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "local_file" "inventory" {
   filename = "${path.module}/../../../inventory.dev"
 }
 
-resource "local_file" "inventory" {
+resource "local_file" "inventory2" {
   content  = templatefile("${path.module}/inventory2.tpl", { groups = var.groups, servers = var.servers, hostname = var.hostname, key_path = var.key_path })
 # filename = "${var.output}
   filename = "${path.module}/../../../inventory2.dev"
