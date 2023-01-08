813c8f8f37b041dc9c35f82d9ab990f55208c529
이윤택 <eeyoontaek@iyuntaeg-ui-MacBookPro.local>
2023-01-08 15:51:30 +0900

    Add inventory2

--- inventory2.tpl
+++ inventory2.tpl
@@ -6,9 +6,6 @@ ${server.name} ansible_ssh_host=${server.ansible_ssh_host} private_ip=${server.p
 %{ endfor ~}
 %{ endfor ~}
 
-[amazon:vars]
-ansible_python_interpreter=/usr/bin/python2
-
 [all:vars]
 ansible_user=${hostname}
 ansible_ssh_private_key_file=${key_path}
--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ resource "local_file" "inventory" {
 # filename = "${var.output}
   filename = "${path.module}/../../../inventory.dev"
 }
+
+resource "local_file" "inventory" {
+  content  = templatefile("${path.module}/inventory2.tpl", { groups = var.groups, servers = var.servers, hostname = var.hostname, key_path = var.key_path })
+# filename = "${var.output}
+  filename = "${path.module}/../../../inventory2.dev"
+}
