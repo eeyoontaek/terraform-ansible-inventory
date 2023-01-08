024cb63615cb6d07051cc79f67ee8cc07f5f54ab
이윤택 <eeyoontaek@iyuntaeg-ui-MacBookPro.local>
2023-01-08 14:39:16 +0900

    FIX path

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "local_file" "inventory" {
   content  = templatefile("${path.module}/inventory.tpl", { groups = var.groups, servers = var.servers, hostname = var.hostname })
-  filename = "${var.output}/hosts"
+# filename = "${var.output}
+  filename = "${path.module}/inventory"
 }
