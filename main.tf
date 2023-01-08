f9c6c3f609a757ed263d3a4afc6a98f5e1b95bec
이윤택 <eeyoontaek@iyuntaeg-ui-MacBookPro.local>
2023-01-08 15:04:35 +0900

    Finish file path

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "local_file" "inventory" {
   content  = templatefile("${path.module}/inventory.tpl", { groups = var.groups, servers = var.servers, hostname = var.hostname, key_path = var.key_path })
 # filename = "${var.output}
-  filename = "${path.module}/../../inventory.dev"
+  filename = "${path.module}/../../../inventory.dev"
 }
